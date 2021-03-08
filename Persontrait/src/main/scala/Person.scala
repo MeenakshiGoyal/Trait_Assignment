88b2c43f8bfe7fba7e9ee6ea2af889d11f6e0f64
MeenakshiGoyal <76804533+MeenakshiGoyal@users.noreply.github.com>
2021-03-08 22:08:13 +0530

    ordered traits within class

--- Persontrait/src/main/scala/Person.scala
+++ Persontrait/src/main/scala/Person.scala
@@ -3,23 +3,23 @@ class Person(val name:String , val age:Int) extends Ordered[Person]{
   def compare(that:Person):Int= {   //Method to do comparison along with their name and age
     if (this.name == that.name) {
       if (this.age < that.age) {
+        -1
+      }
+      else {
         1
       }
-      1
     }
     else{
-      if(this.name.length == that.name.length){
-        1
+      if(this.name.length < that.name.length){
+        -1
       }
       else{
-        0
+        1
       }
     }
 
   }
 
 }
-
-
 
 
